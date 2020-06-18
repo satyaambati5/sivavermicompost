3ba98ab1460772ec15267fe6fc8d54183d8f346e
unknown <satyaambati5@gmail.com>
2020-06-18 21:01:35 +0530

    version 1.0

---
+++ sivavermicompost/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SivavermicompostConfig(AppConfig):
+    name = 'sivavermicompost'
---
+++ sivavermicompost/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.shortcuts import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail as sm
+from django.conf import settings
+# Create your views here.
+
+
+def home(request):
+    return render(request, "index.html",)
+    # return render(request,"index.html")
+
+
+def send(request):
+    name = str(request.POST["your-name"])
+    email = str(request.POST["your-email"])
+    number = int(request.POST["phone"])
+    message = str(request.POST["message"])
+    num = str(number)
+    if len(num) < 10:
+        return HttpResponse(f"please enter a valid mobile number")
+
+    print(number)
+    res = sm(
+        subject=email,
+        message=message + "\n CONTACT:" + str(number),
+        from_email=email,
+        recipient_list=['ambatisathya123@gmail.com'],
+        fail_silently=False,
+    )
+
+    return HttpResponse(f"Email sent to {res} members")
+
+    # email_from = email
+    # # settings.EMAIL_HOST_USER
+    # recipient_list = ['ambatisathya123@gmail.com', ]
+    # send_mail(name, message, email_from, recipient_list)
+
+    # return render(request, "index.html")
